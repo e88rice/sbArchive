6d99564fbc61e04186b71568031707412c425c55
e88rice <guswns8780@naver.com>
2023-11-09 14:35:33 +0900

    Feat #1 - 기본 세팅 2
    # Feat : 이미지 파일 외부 경로 인식 설정 추가_
    지도 마커에 이미지 보이게 설정_
    간판 생성 시 이미지 파일 여러 개 업로드 기능 추가_
    # Design : Header Footer 레이아웃 생성_

--- src/main/java/com/project/sbarchive/controller/signboard/SignboardController.java
+++ src/main/java/com/project/sbarchive/controller/signboard/SignboardController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
 import java.util.List;
 
 @Log4j2
@@ -30,14 +29,15 @@ public class SignboardController {
         return "/signboard/add";
     }
 
+    // 게시글 등록
     @PostMapping(value = "/add")
     public String registerPOST(SignBoardDTO signBoardDTO, List<MultipartFile> files) {
 
-        int signBoardId = signBoardService.add(signBoardDTO);
+        int signBoardId = signBoardService.add(signBoardDTO); // 등록 버튼을 누른 게시글을 DB에 저장하고 방금 등록한 게시물의 id값을 받아옴
 
         System.out.println(signBoardId);
         if(files.size() > 0) {
-            signBoardFileService.add(signBoardId, files);
+            signBoardFileService.add(signBoardId, files); // 받아온 id값에 해당하는 보드의 파일들도 DB에 저장
         }
 
         return "redirect:/index";
