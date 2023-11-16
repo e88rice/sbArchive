2958b4a01df24c0ad7f22d4e2804e8913e99786d
yourim <your-im@naver.com>
2023-11-16 18:47:40 +0900

    Feat #31 - 비밀번호 찾기
    # Feat : 비밀번호 재설정 sendMail, 스프링 시큐리티 핸들러

--- src/main/java/com/project/sbarchive/controller/mail/MailController.java
+++ src/main/java/com/project/sbarchive/controller/mail/MailController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -24,6 +25,17 @@ public class MailController {
         if(mailSenderService.sendMailByAddMember(mailTo)) { // 제대로 발송이 됐다면
             session.setAttribute("confirmKey", mailSenderService.getConfirmKey()); // 현재 발급한 인증키를 세션에 저장하고
             return mailSenderService.getConfirmKey(); // "confirmKey" 반환
+        } else {
+            return "false";
+        }
+    }
+
+
+    @GetMapping("/sendTempPwMail")
+    @ResponseBody
+    public String sendTempPwMail(String mailTo, HttpSession session) throws Exception {
+        if(mailSenderService.sendMailByTempPassword(mailTo)) { // 제대로 발송이 됐다면
+            return "true";
         } else {
             return "false";
         }
