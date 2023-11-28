0c1d4fc8ba078dfa9629741e6e009bd5a3ed0b06
bodolskii <qhtjd2477@naver.com>
2023-11-28 17:10:33 +0900

    Refactor #131 - 보드 css 정리

---
+++ src/main/resources/static/js/board/list.js
@@ -0,0 +1,23 @@
+document.querySelector(".pagination").addEventListener("click",function (e) {
+
+
+    const target = e.target;
+
+    if (target.tagName !== 'A') {
+        return;
+    }
+    const num = parseInt(target.getAttribute("data-num"));
+
+    const formObj = document.querySelector("form");
+
+    formObj.innerHTML += `<input type='hidden' name='page' value='${num}'>`;
+
+    formObj.submit();
+},false)
+
+document.querySelector('.addboard').addEventListener('click',function (e) {
+    location.href = '/board/add'
+});
+document.querySelector('.addnotice').addEventListener('click',function (e) {
+    location.href = '/board/addNotice'
+});
