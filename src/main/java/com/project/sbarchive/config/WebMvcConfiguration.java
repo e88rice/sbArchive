69f18b65c1042fa627d946520f734684311003d6
e88rice <guswns8780@naver.com>
2023-11-28 10:30:32 +0900

    Fix #124 - 파일 저장 경로 변경(2)
    #Fix : 파일 저장 경로가 static 폴더로 교체됨에 따라
    외부 인식 경로를 차단

--- src/main/java/com/project/sbarchive/config/WebMvcConfiguration.java
+++ src/main/java/com/project/sbarchive/config/WebMvcConfiguration.java
@@ -9,18 +9,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfiguration implements WebMvcConfigurer {
 
 
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        // addResourceHandler : 스프링부트에서 확인할 폴더 위치 설정 ( img 폴더 밑에 아무거나 다 들어갈 수 있음 )
-        // addResourceLocations : 실제 시스템의 폴더 위치, 윈도우 시스템의 경우 'file:///경로' 형태로 사용
-
-        registry.addResourceHandler("/img/**")
-                .addResourceLocations(
-                "file:///C:/upload/sbArchive/signboard/",
-                "file:///C:/upload/sbArchive/board/",
-                "file:///C:/upload/sbArchive/user/",
-                "file:///C:/upload/sbArchive/img/"
-                );
-
-    }
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        // addResourceHandler : 스프링부트에서 확인할 폴더 위치 설정 ( img 폴더 밑에 아무거나 다 들어갈 수 있음 )
+//        // addResourceLocations : 실제 시스템의 폴더 위치, 윈도우 시스템의 경우 'file:///경로' 형태로 사용
+//
+//        registry.addResourceHandler("/img/**")
+//                .addResourceLocations(
+//                "file:///C:/upload/sbArchive/signboard/",
+//                "file:///C:/upload/sbArchive/board/",
+//                "file:///C:/upload/sbArchive/user/",
+//                "file:///C:/upload/sbArchive/img/"
+//                );
+//
+//    }
 }
